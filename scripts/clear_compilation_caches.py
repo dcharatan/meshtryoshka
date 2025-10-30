023ff5677b0258a9229073c2f878baf5b38e16ff
David Charatan <david.charatan@gmail.com>
2025-10-30 11:33:09 -0400

    Update compilation cache clearing.

--- scripts/clear_compilation_caches.py
+++ scripts/clear_compilation_caches.py
@@ -1,17 +1,10 @@
 import os
 import shutil
-import sys
 from pathlib import Path
 
 if __name__ == "__main__":
-    executable_path = Path(sys.executable)
-    library_path = executable_path.parents[1] / "lib"
-
-    # Find the Python version.
-    library_python_path = next(iter(library_path.iterdir()))
-
     # Specify the Slang kernels' location.
-    slang_location = library_python_path / "site-packages/triangle_rasterization/slang"
+    slang_location = Path("triangle_rasterization")
 
     # Delete the Slang cache and lock files.
     shutil.rmtree(slang_location / ".slangtorch_cache", True)
